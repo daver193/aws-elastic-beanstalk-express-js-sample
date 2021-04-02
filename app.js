af211dff959a5610990e23f3e1613753d321c880
David Triantafyllou <davidtriant@yahoo.gr>
2021-04-02 20:45:16 +0100

    change message

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Test me'));
+app.get('/', (req, res) => res.send('Testing page Σ!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
